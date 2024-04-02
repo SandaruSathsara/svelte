a6532ddf5493ef322066a133f76af10329fcaa65
SandaruSathsara <sssfernando@students.nsbm.ac.lk>
2024-04-02 11:10:08 +0530

    intial commit

---
+++ src/lib/Components/Footer.svelte
@@ -0,0 +1,3 @@
+<h1>
+    Connected
+</h1>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+import Navbar from "$lib/Components/Navbar.svelte";
+import { navbarData } from "../utils/linkUtil";
+import Footer from "$lib/Components/Footer.svelte";
+
+</script>
+
+<Navbar/>
+<div>
+
+</div>
+<style></style>
+
+<h1>Welcome </h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<Footer/>
+
+
