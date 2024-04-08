a0b5671670b815d03d1f8c507f21ef3e9902a34d
SandaruSathsara <sssfernando@students.nsbm.ac.lk>
2024-04-08 12:16:15 +0530

    women fetch

--- src/routes/Women/+page.svelte
+++ src/routes/Women/+page.svelte
@@ -1,1 +1,31 @@
+<script>
+// @ts-nocheck
+
+
+    import { onMount } from 'svelte';
+    
+    // @ts-ignore
+    let posts = [];
+
+    onMount (async () => {
+
+        const response = await fetch ('https://api.escuelajs.co/api/v1/products')
+        posts = await response.json()
+    
+    })
+
+  </script>
+
+
+  
 <h1>Women</h1>
+
+
+{#each posts as post (post.id) }
+<h2>{post.title}. $ {post.price}</h2>
+<p>{post.description}</p>
+<img  src={post.images} alt=""/>
+<hr />
+{:else}
+<p>loading...</p>
+{/each}
